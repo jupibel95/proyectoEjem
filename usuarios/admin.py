84fe199ee38a6cda243bd0b1f3d1745e5554e5df
alexander <alexgriego15@hotmail.com>
2020-11-26 08:23:15 -0500

    Se agrego la tabla centro asistencial

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Centro_asistencial
+
+class Centro_asistencialAdmin(admin.ModelAdmin):
+    pass
+
 # Register your models here.
+
+admin.site.register(Centro_asistencial, Centro_asistencialAdmin)
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Centro_asistencial(models.Model):
+    nombre = models.CharField(max_length=64)
+    nit = models.IntegerField()
+    numero_de_camas_en_uci = models.IntegerField(max_length=10)
+    numero_de_camas_ocupadas_en_uci = models.IntegerField(max_length=10)
+    numero_de_camas_disponibles_en_uci = models.IntegerField(max_length=10)
